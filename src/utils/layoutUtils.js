ac63753d9d82693831cedfba605ba3f411d09b83
Boyd Duffie <bduffie@fullsail.com>
2023-04-16 13:38:08 -0400

    Add `getCenterOfScreen` util

--- src/utils/layoutUtils.js
+++ src/utils/layoutUtils.js
@@ -9,3 +9,11 @@ export const getScreenDimensions = () => ({
   width: getScreenWidth(),
   height: getScreenHeight(),
 });
+
+// Window locations
+export const getCenterOfScreen = () => {
+  return {
+    x: getScreenWidth() / 2,
+    y: getScreenHeight() / 2,
+  };
+};
