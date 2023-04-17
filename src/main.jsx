cb801f0ff8ab9e8902aaa0f4cdefcc68f3506763
Boyd Duffie <bduffie@fullsail.com>
2023-04-17 09:21:14 -0400

    Add fixme for strictmode concerns

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
+  // FIXME: We need to find a way to ensure the Game is only mounted once
   <React.StrictMode>
     <App />
   </React.StrictMode>
