8aa5c3abd2073a631c41464471069667b93dae65
Boyd Duffie <bduffie@fullsail.com>
2023-04-17 09:20:58 -0400

    Empty App.jsx (moving away from RPF)

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,15 @@
 import React from "react";
 
-import { Game, Scene } from "react-phaser-fiber";
-import MyScene from "@scenes/MyScene";
-
-import { getScreenDimensions } from "@utils/layoutUtils";
+import { getScreenDimensions, getCenterOfScreen } from "@utils/layoutUtils";
 
 function App() {
   const { width, height } = getScreenDimensions();
+  const { x: centerX, y: centerY } = getCenterOfScreen();
 
   // TODO: When window is resized, update the width and height of the game
   // TODO: Use global state to track window dimensions
 
-  const preload = (scene) => {
-    scene.load.image("logo", "https://assets.codepen.io/388466/logo.png");
-  };
-
-  return (
-    <Game
-      width={width}
-      height={height}
-      backgroundColor={0x1099bb}
-      onPreload={preload}
-    >
-      {/* Our custom-implemented scenes get wrapped with a Scene to establish their `key` and `active-states` */}
-      <Scene sceneKey="main" active>
-        <MyScene />
-      </Scene>
-    </Game>
-  );
+  return <div>Help me</div>;
 }
 
 export default App;
