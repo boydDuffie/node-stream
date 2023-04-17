959e001d5fb9f92e7ab07315b3d94bc30c9d94f1
Boyd Duffie <bduffie@fullsail.com>
2023-04-17 09:20:45 -0400

    Remove content from MyScene (moving away from RPF)

--- src/scenes/MyScene.jsx
+++ src/scenes/MyScene.jsx
@@ -1,23 +1,3 @@
-import { useScene, useGame, Sprite } from "react-phaser-fiber";
-
-const MyScene = () => {
-  const scene = useScene();
-  const game = useGame();
-
-  React.useEffect(() => {
-    // You can access the Phaser scene and game instances here
-  }, [scene, game]);
-
-  return (
-    <Sprite
-      x={100}
-      y={100}
-      texture="logo"
-      onClick={() => {
-        console.log("Sprite clicked");
-      }}
-    />
-  );
-};
+const MyScene = () => {};
 
 export default MyScene;
